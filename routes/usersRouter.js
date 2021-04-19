068d7317680370c86f9ceac8027ca7d62e76c301
mateusntn <mateusantoniodev@gmail.com>
2021-04-19 15:38:07 -0300

    Creates the usersRouter

---
+++ routes/usersRouter.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const usersController = require('../controllers/usersController');
+
+router.get('/', usersController.index);
+router.post('/', usersController.create);
+router.put('/:id', usersController.update);
+router.delete('/:id', usersController.delete);
+
+module.exports = router;
