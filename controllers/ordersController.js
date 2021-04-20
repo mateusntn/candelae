b9f664e8f9b2ab5acbb1b996879e9c771b0096ac
mateusntn <mateusantoniodev@gmail.com>
2021-04-20 16:15:02 -0300

    Creates the update and delete methods of orders

--- controllers/ordersController.js
+++ controllers/ordersController.js
@@ -10,6 +10,23 @@ const ordersController = {
         const { shipping, amount, users_id, formPayments_id, order_status } = req.body;
         const newOrder = await Order.create({shipping, amount, users_id, formPayments_id, order_status });
         return res.json(newOrder);
+    },
+
+    update: async (req, res) => {
+        const {id} = req.params;
+        const {shipping, amount, users_id, formPayments_id, order_status} = req.body;
+
+        const orderUpdated = await Order.update({shipping, amount, users_id, formPayments_id, order_status}, {where: {id}});
+
+        return res.json(orderUpdated);
+    },
+
+    delete: async (req, res) => {
+        const {id} = req.params;
+        
+        const orderDeleted = await Order.destroy({where: {id}});
+
+        return res.json(orderDeleted);
     }
 }
 
