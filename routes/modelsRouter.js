06b2b97bbf4597114647ad6856fb5959fde9711e
Letícia Paulino <karlaleticiatavares@gmail.com>
2021-04-22 17:32:33 -0300

    Creates put router

--- routes/modelsRouter.js
+++ routes/modelsRouter.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const modelsController = require('../controllers/modelsController');
 
 router.get('/', modelsController.index);
+router.put('/:id', modelsController.update);
 router.post('/', modelsController.create);
 router.delete('/:id', modelsController.delete);
 
